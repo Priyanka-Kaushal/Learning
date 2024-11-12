a4502c7fcd5bb0b12d9c83944d25619d0de46851
Priyanka <priyankakaushal2308@gmail.com>
2024-11-12 14:35:01 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,19 +3,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
 import Protected from "./components/Protected";
 
-// import CardList from "./roboApp/Cardlist";
-// import {robots} from './roboApp/robots';
-// import Search from "./roboApp/searchBoxComponent";
-// import React, {useState} from "react";
-// import MainPageItems from "./practices/mainPageForProducts.js"
-// import StateUse from "./hooks/useStatePractice";
-
-
-
-
-// import MatLogin from "./components/MatLogin";
-// import MainFunction from "./practices/mainFunction";
-
 function App(){
     return (
            <BrowserRouter>
